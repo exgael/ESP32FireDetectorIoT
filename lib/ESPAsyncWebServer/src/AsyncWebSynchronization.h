677c38117646fbee23b5a6f124fbc813864925d6
exgael <benoitbarbier.dev@gmail.com>
2024-11-08 13:15:38 +0100

    fix(lib): did a global cmd again...

--- lib/ESPAsyncWebServer/src/AsyncWebSynchronization.h
+++ lib/ESPAsyncWebServer/src/AsyncWebSynchronization.h
@@ -8,69 +8,80 @@
 #ifdef ESP32
 
 // This is the ESP32 version of the Sync Lock, using the FreeRTOS Semaphore
-class AsyncWebLock {
-   private:
-    SemaphoreHandle_t _lock;
-    mutable void *_lockedBy;
+class AsyncWebLock
+{
+private:
+  SemaphoreHandle_t _lock;
+  mutable void *_lockedBy;
 
-   public:
-    AsyncWebLock() {
-        _lock = xSemaphoreCreateBinary();
-        _lockedBy = NULL;
-        xSemaphoreGive(_lock);
+public:
+  AsyncWebLock() {
+    _lock = xSemaphoreCreateBinary();
+    _lockedBy = NULL;
+    xSemaphoreGive(_lock);
+  }
+
+  ~AsyncWebLock() {
+    vSemaphoreDelete(_lock);
+  }
+
+  bool lock() const {
+    extern void *pxCurrentTCB;
+    if (_lockedBy != pxCurrentTCB) {
+      xSemaphoreTake(_lock, portMAX_DELAY);
+      _lockedBy = pxCurrentTCB;
+      return true;
     }
+    return false;
+  }
 
-    ~AsyncWebLock() { vSemaphoreDelete(_lock); }
-
-    bool lock() const {
-        extern void *pxCurrentTCB;
-        if (_lockedBy != pxCurrentTCB) {
-            xSemaphoreTake(_lock, portMAX_DELAY);
-            _lockedBy = pxCurrentTCB;
-            return true;
-        }
-        return false;
-    }
-
-    void unlock() const {
-        _lockedBy = NULL;
-        xSemaphoreGive(_lock);
-    }
+  void unlock() const {
+    _lockedBy = NULL;
+    xSemaphoreGive(_lock);
+  }
 };
 
 #else
 
 // This is the 8266 version of the Sync Lock which is currently unimplemented
-class AsyncWebLock {
-   public:
-    AsyncWebLock() {}
+class AsyncWebLock
+{
 
-    ~AsyncWebLock() {}
+public:
+  AsyncWebLock() {
+  }
 
-    bool lock() const { return false; }
+  ~AsyncWebLock() {
+  }
 
-    void unlock() const {}
+  bool lock() const {
+    return false;
+  }
+
+  void unlock() const {
+  }
 };
 #endif
 
-class AsyncWebLockGuard {
-   private:
-    const AsyncWebLock *_lock;
+class AsyncWebLockGuard
+{
+private:
+  const AsyncWebLock *_lock;
 
-   public:
-    AsyncWebLockGuard(const AsyncWebLock &l) {
-        if (l.lock()) {
-            _lock = &l;
-        } else {
-            _lock = NULL;
-        }
+public:
+  AsyncWebLockGuard(const AsyncWebLock &l) {
+    if (l.lock()) {
+      _lock = &l;
+    } else {
+      _lock = NULL;
     }
+  }
 
-    ~AsyncWebLockGuard() {
-        if (_lock) {
-            _lock->unlock();
-        }
+  ~AsyncWebLockGuard() {
+    if (_lock) {
+      _lock->unlock();
     }
+  }
 };
 
-#endif  // ASYNCWEBSYNCHRONIZATION_H_
+#endif // ASYNCWEBSYNCHRONIZATION_H_
--- lib/ESPAsyncWebServer/src/WebServer.cpp
+++ lib/ESPAsyncWebServer/src/WebServer.cpp
@@ -21,154 +21,173 @@
 #include "ESPAsyncWebServer.h"
 #include "WebHandlerImpl.h"
 
-bool ON_STA_FILTER(AsyncWebServerRequest* request) { return WiFi.localIP() == request->client()->localIP(); }
+bool ON_STA_FILTER(AsyncWebServerRequest *request) {
+  return WiFi.localIP() == request->client()->localIP();
+}
 
-bool ON_AP_FILTER(AsyncWebServerRequest* request) { return WiFi.localIP() != request->client()->localIP(); }
+bool ON_AP_FILTER(AsyncWebServerRequest *request) {
+  return WiFi.localIP() != request->client()->localIP();
+}
+
 
 AsyncWebServer::AsyncWebServer(uint16_t port)
-    : _server(port),
-      _rewrites(LinkedList<AsyncWebRewrite*>([](AsyncWebRewrite* r) { delete r; })),
-      _handlers(LinkedList<AsyncWebHandler*>([](AsyncWebHandler* h) { delete h; })) {
-    _catchAllHandler = new AsyncCallbackWebHandler();
-    if (_catchAllHandler == NULL) return;
-    _server.onClient(
-        [](void* s, AsyncClient* c) {
-            if (c == NULL) return;
-            c->setRxTimeout(3);
-            AsyncWebServerRequest* r = new AsyncWebServerRequest((AsyncWebServer*)s, c);
-            if (r == NULL) {
-                c->close(true);
-                c->free();
-                delete c;
-            }
-        },
-        this);
+  : _server(port)
+  , _rewrites(LinkedList<AsyncWebRewrite*>([](AsyncWebRewrite* r){ delete r; }))
+  , _handlers(LinkedList<AsyncWebHandler*>([](AsyncWebHandler* h){ delete h; }))
+{
+  _catchAllHandler = new AsyncCallbackWebHandler();
+  if(_catchAllHandler == NULL)
+    return;
+  _server.onClient([](void *s, AsyncClient* c){
+    if(c == NULL)
+      return;
+    c->setRxTimeout(3);
+    AsyncWebServerRequest *r = new AsyncWebServerRequest((AsyncWebServer*)s, c);
+    if(r == NULL){
+      c->close(true);
+      c->free();
+      delete c;
+    }
+  }, this);
 }
 
-AsyncWebServer::~AsyncWebServer() {
-    reset();
-    end();
-    if (_catchAllHandler) delete _catchAllHandler;
+AsyncWebServer::~AsyncWebServer(){
+  reset();  
+  end();
+  if(_catchAllHandler) delete _catchAllHandler;
 }
 
-AsyncWebRewrite& AsyncWebServer::addRewrite(AsyncWebRewrite* rewrite) {
-    _rewrites.add(rewrite);
-    return *rewrite;
+AsyncWebRewrite& AsyncWebServer::addRewrite(AsyncWebRewrite* rewrite){
+  _rewrites.add(rewrite);
+  return *rewrite;
 }
 
-bool AsyncWebServer::removeRewrite(AsyncWebRewrite* rewrite) { return _rewrites.remove(rewrite); }
-
-AsyncWebRewrite& AsyncWebServer::rewrite(const char* from, const char* to) {
-    return addRewrite(new AsyncWebRewrite(from, to));
+bool AsyncWebServer::removeRewrite(AsyncWebRewrite *rewrite){
+  return _rewrites.remove(rewrite);
 }
 
-AsyncWebHandler& AsyncWebServer::addHandler(AsyncWebHandler* handler) {
-    _handlers.add(handler);
-    return *handler;
+AsyncWebRewrite& AsyncWebServer::rewrite(const char* from, const char* to){
+  return addRewrite(new AsyncWebRewrite(from, to));
 }
 
-bool AsyncWebServer::removeHandler(AsyncWebHandler* handler) { return _handlers.remove(handler); }
-
-void AsyncWebServer::begin() {
-    _server.setNoDelay(true);
-    _server.begin();
+AsyncWebHandler& AsyncWebServer::addHandler(AsyncWebHandler* handler){
+  _handlers.add(handler);
+  return *handler;
 }
 
-void AsyncWebServer::end() { _server.end(); }
+bool AsyncWebServer::removeHandler(AsyncWebHandler *handler){
+  return _handlers.remove(handler);
+}
+
+void AsyncWebServer::begin(){
+  _server.setNoDelay(true);
+  _server.begin();
+}
+
+void AsyncWebServer::end(){
+  _server.end();
+}
 
 #if ASYNC_TCP_SSL_ENABLED
-void AsyncWebServer::onSslFileRequest(AcSSlFileHandler cb, void* arg) { _server.onSslFileRequest(cb, arg); }
+void AsyncWebServer::onSslFileRequest(AcSSlFileHandler cb, void* arg){
+  _server.onSslFileRequest(cb, arg);
+}
 
-void AsyncWebServer::beginSecure(const char* cert, const char* key, const char* password) {
-    _server.beginSecure(cert, key, password);
+void AsyncWebServer::beginSecure(const char *cert, const char *key, const char *password){
+  _server.beginSecure(cert, key, password);
 }
 #endif
 
-void AsyncWebServer::_handleDisconnect(AsyncWebServerRequest* request) { delete request; }
+void AsyncWebServer::_handleDisconnect(AsyncWebServerRequest *request){
+  delete request;
+}
 
-void AsyncWebServer::_rewriteRequest(AsyncWebServerRequest* request) {
-    for (const auto& r : _rewrites) {
-        if (r->match(request)) {
-            request->_url = r->toUrl();
-            request->_addGetParams(r->params());
-        }
+void AsyncWebServer::_rewriteRequest(AsyncWebServerRequest *request){
+  for(const auto& r: _rewrites){
+    if (r->match(request)){
+      request->_url = r->toUrl();
+      request->_addGetParams(r->params());
     }
+  }
 }
 
-void AsyncWebServer::_attachHandler(AsyncWebServerRequest* request) {
-    for (const auto& h : _handlers) {
-        if (h->filter(request) && h->canHandle(request)) {
-            request->setHandler(h);
-            return;
-        }
+void AsyncWebServer::_attachHandler(AsyncWebServerRequest *request){
+  for(const auto& h: _handlers){
+    if (h->filter(request) && h->canHandle(request)){
+      request->setHandler(h);
+      return;
     }
-
-    request->addInterestingHeader("ANY");
-    request->setHandler(_catchAllHandler);
+  }
+  
+  request->addInterestingHeader("ANY");
+  request->setHandler(_catchAllHandler);
 }
 
-AsyncCallbackWebHandler& AsyncWebServer::on(const char* uri, WebRequestMethodComposite method,
-                                            ArRequestHandlerFunction onRequest, ArUploadHandlerFunction onUpload,
-                                            ArBodyHandlerFunction onBody) {
-    AsyncCallbackWebHandler* handler = new AsyncCallbackWebHandler();
-    handler->setUri(uri);
-    handler->setMethod(method);
-    handler->onRequest(onRequest);
-    handler->onUpload(onUpload);
-    handler->onBody(onBody);
-    addHandler(handler);
-    return *handler;
+
+AsyncCallbackWebHandler& AsyncWebServer::on(const char* uri, WebRequestMethodComposite method, ArRequestHandlerFunction onRequest, ArUploadHandlerFunction onUpload, ArBodyHandlerFunction onBody){
+  AsyncCallbackWebHandler* handler = new AsyncCallbackWebHandler();
+  handler->setUri(uri);
+  handler->setMethod(method);
+  handler->onRequest(onRequest);
+  handler->onUpload(onUpload);
+  handler->onBody(onBody);
+  addHandler(handler);
+  return *handler;
 }
 
-AsyncCallbackWebHandler& AsyncWebServer::on(const char* uri, WebRequestMethodComposite method,
-                                            ArRequestHandlerFunction onRequest, ArUploadHandlerFunction onUpload) {
-    AsyncCallbackWebHandler* handler = new AsyncCallbackWebHandler();
-    handler->setUri(uri);
-    handler->setMethod(method);
-    handler->onRequest(onRequest);
-    handler->onUpload(onUpload);
-    addHandler(handler);
-    return *handler;
+AsyncCallbackWebHandler& AsyncWebServer::on(const char* uri, WebRequestMethodComposite method, ArRequestHandlerFunction onRequest, ArUploadHandlerFunction onUpload){
+  AsyncCallbackWebHandler* handler = new AsyncCallbackWebHandler();
+  handler->setUri(uri);
+  handler->setMethod(method);
+  handler->onRequest(onRequest);
+  handler->onUpload(onUpload);
+  addHandler(handler);
+  return *handler;
 }
 
-AsyncCallbackWebHandler& AsyncWebServer::on(const char* uri, WebRequestMethodComposite method,
-                                            ArRequestHandlerFunction onRequest) {
-    AsyncCallbackWebHandler* handler = new AsyncCallbackWebHandler();
-    handler->setUri(uri);
-    handler->setMethod(method);
-    handler->onRequest(onRequest);
-    addHandler(handler);
-    return *handler;
+AsyncCallbackWebHandler& AsyncWebServer::on(const char* uri, WebRequestMethodComposite method, ArRequestHandlerFunction onRequest){
+  AsyncCallbackWebHandler* handler = new AsyncCallbackWebHandler();
+  handler->setUri(uri);
+  handler->setMethod(method);
+  handler->onRequest(onRequest);
+  addHandler(handler);
+  return *handler;
 }
 
-AsyncCallbackWebHandler& AsyncWebServer::on(const char* uri, ArRequestHandlerFunction onRequest) {
-    AsyncCallbackWebHandler* handler = new AsyncCallbackWebHandler();
-    handler->setUri(uri);
-    handler->onRequest(onRequest);
-    addHandler(handler);
-    return *handler;
+AsyncCallbackWebHandler& AsyncWebServer::on(const char* uri, ArRequestHandlerFunction onRequest){
+  AsyncCallbackWebHandler* handler = new AsyncCallbackWebHandler();
+  handler->setUri(uri);
+  handler->onRequest(onRequest);
+  addHandler(handler);
+  return *handler;
 }
 
-AsyncStaticWebHandler& AsyncWebServer::serveStatic(const char* uri, fs::FS& fs, const char* path,
-                                                   const char* cache_control) {
-    AsyncStaticWebHandler* handler = new AsyncStaticWebHandler(uri, fs, path, cache_control);
-    addHandler(handler);
-    return *handler;
+AsyncStaticWebHandler& AsyncWebServer::serveStatic(const char* uri, fs::FS& fs, const char* path, const char* cache_control){
+  AsyncStaticWebHandler* handler = new AsyncStaticWebHandler(uri, fs, path, cache_control);
+  addHandler(handler);
+  return *handler;
 }
 
-void AsyncWebServer::onNotFound(ArRequestHandlerFunction fn) { _catchAllHandler->onRequest(fn); }
-
-void AsyncWebServer::onFileUpload(ArUploadHandlerFunction fn) { _catchAllHandler->onUpload(fn); }
-
-void AsyncWebServer::onRequestBody(ArBodyHandlerFunction fn) { _catchAllHandler->onBody(fn); }
-
-void AsyncWebServer::reset() {
-    _rewrites.free();
-    _handlers.free();
-
-    if (_catchAllHandler != NULL) {
-        _catchAllHandler->onRequest(NULL);
-        _catchAllHandler->onUpload(NULL);
-        _catchAllHandler->onBody(NULL);
-    }
+void AsyncWebServer::onNotFound(ArRequestHandlerFunction fn){
+  _catchAllHandler->onRequest(fn);
 }
+
+void AsyncWebServer::onFileUpload(ArUploadHandlerFunction fn){
+  _catchAllHandler->onUpload(fn);
+}
+
+void AsyncWebServer::onRequestBody(ArBodyHandlerFunction fn){
+  _catchAllHandler->onBody(fn);
+}
+
+void AsyncWebServer::reset(){
+  _rewrites.free();
+  _handlers.free();
+  
+  if (_catchAllHandler != NULL){
+    _catchAllHandler->onRequest(NULL);
+    _catchAllHandler->onUpload(NULL);
+    _catchAllHandler->onBody(NULL);
+  }
+}
+
